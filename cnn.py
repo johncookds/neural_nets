6a5c58c8fac2453433d4453423f5e36f2c216c5f
John Cook <johncookds@gmail.com>
2017-10-05 10:50:00 -0400

    Create cnn.py

---
+++ cnn.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import os
+import numpy as np
+import scipy as sp
+import sklearn.ensemble as ske
+from sklearn.cross_validation import cross_val_score
+import tensorflow as tf
+from __future__ import print_function
+import inspect
+from collections import OrderedDict
+
+class classification_cnn(object):
+
+	def __init__(self):
+		pass
+	
+	
